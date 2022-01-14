335a26e1ff3cad5fe0e0a6e84202863c40b4510c
NathalyEspinal <espinalnathaly@gmail.com>
2022-01-13 22:26:51 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+
+
+function writeCards(names, eventName) {
+  let array = []
+  for (let i=0; i<names.length; i++) {
+
+  array.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return array
+  }
+
+
+
+function countDown() {
+  let i= 10;
+  while (i >= 0) {
+    console.log(i--);
+}
+}
