83cb75f0add20cb1a2ca2ee1f7f1f629349b94f5
NathalyEspinal <espinalnathaly@gmail.com>
2022-02-24 19:58:51 -0600

    commit tree 2ca3b47e4f072058b9e32ef567ccfdcc4f1576a3

--- index.js
+++ index.js
@@ -1,19 +1,86 @@
 
+//solution one: forEach 
 
-function writeCards(names, eventName) {
-  let array = []
-  for (let i=0; i<names.length; i++) {
+// function numberedList(array){
+//     array.forEach((element, index) => {
+//        console.log(`${index + 1}. ${element}`)
+//     }) 
+//   }
+  
+//   //solution two: for loop
+  
+//   function numberedList(array){
+//     for(let i = 0; i < array.length; i++){
+//       console.log(`${i + 1}. ${array[i]}`)
+//     }
+//   }
+  
+//   //solution three: for of
+  
+//   function numberedList(array){
+//     for(let element of array){
+//       console.log(`${array.indexOf(element) + 1}. ${element}`)
+//     }
+//   }
 
-  array.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
-  }
-  return array
-  }
+// for (let i=5; i<= 20; i+=1){
+//     console.log(i)
+// }
 
 
 
-function countDown() {
-  let i= 10;
-  while (i >= 0) {
-    console.log(i--);
+
+function writeCards(nameArray, eventName){
+    let messages =[]
+        for (let i=0; i<nameArray.length; i++){
+         messages.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!` ) 
+          
+    }
+    return messages
 }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
